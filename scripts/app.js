4da941aa2198f7504e9a2f8dc2a5add981cf2951
pour68 <pour68@outlook.com>
2024-12-29 23:58:02 +0330

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,62 @@
+const swiper = new Swiper(".swiper-container", {
+    direction: "vertical",
+    effect: "fade",
+    speed: 1000,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    mousewheel: {
+        invert: false,
+        forceToAxis: false,
+        thresholdDelta: 50,
+        sensitivity: 1,
+    },
+    on: {
+        slideChange: function () {
+            this.slides.forEach((slide) => {
+                let background = slide.querySelector(".background");
+                if (background) {
+                    background.classList.remove("animation");
+                }
+            });
+            let activeSlide = this.slides[this.activeIndex];
+            let background = activeSlide.querySelector(".background");
+            if (background) {
+                background.classList.add("animation");
+            }
+        },
+    },
+});
+
+
+const vfxSwiper = new Swiper(".vfx__swiper", {
+    effect: "coverflow",
+    speed: 900,
+    loop: true,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+    spaceBetween: 900,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 300,
+        modifier: 1,
+        slideShadows: false
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    mousewheel: {
+        invert: false,
+        thresholdDelta: 50,
+        sensitivity: 1
+    }
+});
